b8f874f05f58972c9471a878536ec9c969c3af8b
kim <157290883+pengmsgt@users.noreply.github.com>
2024-02-22 13:37:13 +0800

    Add files via upload

---
+++ enterhp.js
@@ -0,0 +1,16 @@
+// JavaScript Document
+
+$(function(){
+	var mySwiper = new Swiper('.swiper-container',{
+		loop: true,
+		autoplay : 3000,
+		speed:2000,
+		spaceBetween:10,
+		slidesPerView: 4,
+		slidesPerViewFit : false,
+		mousewheelControl : false,
+		autoplayDisableOnInteraction : false,
+		loopedSlides :8
+		
+	})
+})
